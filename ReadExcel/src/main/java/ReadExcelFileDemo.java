d29a0d2727461de17014007b4d7bc68bc2e5f672
Itz-Me-Prajeena <prajeena.me@gmail.com>
2023-07-13 10:40:07 +0530

    MavenExcel 13/7/23

---
+++ ReadExcel/src/main/java/ReadExcelFileDemo.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;  
+
+public class ReadExcelFileDemo  
+{  
+public static void main(String args[]) throws IOException  
+{  
+//obtaining input bytes from a file  
+FileInputStream fis=new FileInputStream(new File("C:\\Users\\praje\\Documents\\Book2.xlsx"));  
+//creating workbook instance that refers to .xls file  
+XSSFWorkbook wb=new XSSFWorkbook(fis);   
+//creating a Sheet object to retrieve the object  
+XSSFSheet sheet=wb.getSheetAt(0);  
+//evaluating cell type   
+FormulaEvaluator formulaEvaluator=wb.getCreationHelper().createFormulaEvaluator();  
+for(Row row: sheet)     //iteration over row using for each loop  
+{  
+for(Cell cell: row)    //iteration over cell using for each loop  
+{  
+System.out.println(cell);
+}  
+
+System.out.println();  
+}  
+}  
+}  
